f01f2c4e7cf4aab23f841f5b3fdc0340c029cdc0
Mateusz Wójciak <matwojciak@gmail.com>
2020-12-16 21:27:57 +0100

    19.2 findById v2

--- src/main/java/com/crud/tasks/service/DbService.java
+++ src/main/java/com/crud/tasks/service/DbService.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -17,7 +18,7 @@ public class DbService {
         return repository.findAll();
     }
 
-    public Task getTaskId(int id) {
-        return getAllTasks().get(id);
+    public Optional<Task> getTaskId(final Long id) {
+        return repository.findById(id);
     }
 }
