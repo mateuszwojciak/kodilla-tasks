a6658273742b1331213e8919fa672151ac551665
Mateusz Wójciak <matwojciak@gmail.com>
2021-03-18 21:49:40 +0100

    29.2 Pokrycie kodu

--- src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTestSuite.java
+++ src/test/java/com/crud/tasks/trello/mapper/TrelloMapperTestSuite.java
@@ -1,7 +1,7 @@
 package com.crud.tasks.trello.mapper;
 
 import com.crud.tasks.domain.*;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
