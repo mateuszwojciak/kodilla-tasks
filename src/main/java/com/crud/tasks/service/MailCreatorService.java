da79b7014bec331743d3299a488aa7d2e5fde238
Mateusz Wójciak <matwojciak@gmail.com>
2021-03-29 17:54:11 +0200

    32.3 Instrukcje warunkowe w szablonie

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,6 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.repository.TaskRepository;
 import com.crud.tasks.trello.config.AdminConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -7,8 +8,14 @@ import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class MailCreatorService {
+
+    @Autowired
+    private TaskRepository taskRepository;
 
     @Autowired
     private AdminConfig adminConfig;
@@ -18,15 +25,38 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
+        List<String> functionality = new ArrayList<>();
+        functionality.add("You can manage your tasks");
+        functionality.add("Provides connection with Trello Account");
+        functionality.add("Application allows sending tasks to Trello");
+
         Context context = new Context();
         context.setVariable("message", message);
-        context.setVariable("previewMessage", "here is preview message"); //new
-        context.setVariable("goodbyeMessage", "Goodbye!");               //new
+        context.setVariable("previewMessage", "here is preview message");
+        context.setVariable("goodbyeMessage", "Goodbye!");
         context.setVariable("tasks_url", "http://localhost:8888/crud");
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("companyDetails", "here is company details info");   //new
+        context.setVariable("companyDetails", "here is company details info");
+        context.setVariable("show_button", true);
+        context.setVariable("is_friend", false);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("application_functionality", functionality);
+        return templateEngine.process("mail/created-trello-card-mail", context);
+    }
 
+    public String buildDailyInformationEmail() {
+        long numberOfTasks = taskRepository.count();
+        String dailyMessage = "Liczba zadań dostępnych w bazie danych na dziś: " + numberOfTasks;
+        Context context = new Context();
+        context.setVariable("message", dailyMessage);
+        context.setVariable("welcomeMessage", "Witaj!");
+        context.setVariable("goodbyeMessage", "Z poważaniem, ");
+        context.setVariable("mailSender", "Zespół ds Powiadomień Dziennych");
+        context.setVariable("companyDetails", "here is company details info");
+        context.setVariable("is_friend", true);
+        context.setVariable("admin_config", adminConfig);
+        context.setVariable("show_functionality", false);
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
